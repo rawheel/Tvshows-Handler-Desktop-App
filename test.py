42a4a47c1c12841384750d64a8904b3d0abf88b6
rawheel <raheeelalisiddiqui@gmail.com>
2021-06-03 18:51:15 +0500

    live example

---
+++ test.py
@@ -0,0 +1,8 @@
+from TvshowsData import get_Netflix_action_data,get_Disney_action_data,get_Netflix_scifi_data,get_Disney_scifi_data
+
+'''print(get_Netflix_action_data())
+print(get_Disney_action_data())
+print(get_Netflix_scifi_data())
+print(get_Disney_scifi_data())'''
+'''a = get_Netflix_action_data()
+[print(i) for i in a]'''
